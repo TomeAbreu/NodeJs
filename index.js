52ca22a9c890f339c5c27b2341df9fe73a6169e8
Tome Abreu <tomeabreu05@gmail.com>
2022-10-19 21:33:37 +0100

    Exercise 2.4 Completed

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+  persons = persons.filter((person) => person.id !== id);
   response.status(204).end();
 });
 
