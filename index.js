0e926c4547addecc80f4ca40dec40c58b209da30
Tome Abreu <tomeabreu05@gmail.com>
2022-10-22 21:09:30 +0100

    Exercise 3.16 Completed

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ const morgan = require("morgan");
 //Import model Person from models
 const Person = require("./models/person");
 
+//App to use Middleware status to go to folder build and use endpoints from there(front-end app)
+app.use(express.static("build"));
+
+//App to use Middleware to parse all request to JSON
+app.use(express.json());
+
+//App to use Middleware CORS to accept front end app connection
+app.use(cors());
+
 //Custom Middleware definition for all requests (needs to be implemented before requests)
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -23,19 +32,10 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-morgan.token("body", (req, res) => JSON.stringify(req.body));
-
-//App to use Middleware to parse all request to JSON
-app.use(express.json());
-
-//App to use Middleware CORS to accept front end app connection
-app.use(cors());
-
-//App to use Middleware status to go to folder build and use endpoints from there(front-end app)
-app.use(express.static("build"));
-
 //App to use our Custom Middleware
 app.use(requestLogger);
+
+morgan.token("body", (req, res) => JSON.stringify(req.body));
 
 //App to use Middleware Morgan
 app.use(morgan(":method :url :status :response-time ms :body"));
@@ -56,7 +56,7 @@ app.get("/api/persons", (request, response) => {
 });
 
 //Get specific person
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const personID = request.params.id;
   Person.findById(personID)
     .then((person) => {
@@ -67,15 +67,17 @@ app.get("/api/persons/:id", (request, response) => {
       }
     })
     .catch((error) => {
-      return response.status(400).send({ error: "malformatted id" });
+      //If the next function is called with a parameter,
+      //then the execution will continue to the error handler middleware
+      next(error);
     });
 });
 
 //Delete person by Id
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   console.log("Request param ID:", request.params.id);
 
-  Person.deleteOne({ _id: request.params.id })
+  Person.findByIdAndDelete(request.params.id)
     .then((deletedPerson) => {
       if (deletedPerson.deletedCount > 0) {
         return response.status(204).end();
@@ -84,14 +86,12 @@ app.delete("/api/persons/:id", (request, response) => {
       }
     })
     .catch((error) => {
-      return response
-        .status(400)
-        .send({ error: "Error in request to delete person" });
+      next(error);
     });
 });
 
 //Create entrie in phonebook
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body) {
@@ -129,13 +129,23 @@ app.post("/api/persons", (request, response) => {
     .save()
     .then((result) => response.status(201).json(newPerson))
     .catch((error) => {
-      return response
-        .status(404)
-        .send(
-          "Some error occurred while creating new person in phonebook database"
-        );
+      next(error);
     });
 });
+
+//Error Handler Middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+//App to use Middleware Error Handler(must be the last middleware loadded)
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
